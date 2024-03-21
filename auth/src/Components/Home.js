8e238399df1a3c0bc80c44610791463121cf5365
machineke <markarapsoi72@gmail.com>
2024-03-21 20:23:32 +0300

    added android

--- auth/src/Components/Home.js
+++ auth/src/Components/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
     <>
       {user ? (
         <div className="container">
-          <h1>Venus Project</h1>
+          <h1>Login App</h1>
           <p>Welcome {user.username}!</p>
              <Link to="" className="link" onClick={handleLogout}>
               <div className="logo2">
--- auth/src/Components/Login.js
+++ auth/src/Components/Login.js
@@ -30,7 +30,6 @@ const Login = () => {
       <div>
         <img className="loadingImg" src="https://cdn.pixabay.com/animation/2023/10/08/03/19/03-19-26-213_512.gif" />
                 <h1>Loading ...</h1>
-
       </div>
     );
   }
